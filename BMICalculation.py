2e5dd0bc3da166a273dd9bf0a3704994528c0a3d
JackCaff <JackCaff@users.noreply.github.com>
2020-01-22 20:44:51 +0000

    Added new file for Calculating the users BMI

---
+++ BMICalculation.py
@@ -0,0 +1,12 @@
+# Program will allow user to enter height in (CM) and weight in (KG) and calculate their BMI.
+
+Weight = float(input("Enter your Weight in Kg: "))  #Allows user to enter Weight
+Height = float(input("Enter your Height in Cm: "))  #Allows user to enter Height
+
+Meters_squared = ((Height * Height) / 100) #Converts height entered in CM to Metered squared
+
+BMI = (Weight / Meters_squared) * 100
+
+new_BMI = round(BMI, 2) #Rounds BMI value too 2 decimals places
+
+print("Your BMI is", new_BMI,) #Calculates the users BMI 
